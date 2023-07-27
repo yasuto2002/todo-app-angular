506e2d991a774434b45b273c6857a7e20c5b50cd
yasuto <fujiya0101@gmail.com>
2023-07-27 18:05:36 +0900

    add:編集ボタンUI修正

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { TodoListComponent } from './todo/todo-list/todo-list.component';
+import { AppComponent } from './app.component';
 
 const routes: Routes = [
-  { path: 'todo', component: TodoListComponent }
+  { path: 'todo', component: TodoListComponent },
 ];
 
 @NgModule({
--- src/app/todo/todo.module.ts
+++ src/app/todo/todo.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TodoListComponent } from './todo-list/todo-list.component';
 import {MatTableModule} from '@angular/material/table';
+import {MatButtonModule} from '@angular/material/button';
 
 @NgModule({
   declarations: [
@@ -9,7 +10,8 @@ import {MatTableModule} from '@angular/material/table';
   ],
   imports: [
     CommonModule,
-    MatTableModule
+    MatTableModule,
+    MatButtonModule
   ]
 })
 export class TodoModule { }
