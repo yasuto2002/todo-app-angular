7b32787f957f0bf6542ba0277dda5d0b8fb15dee
yasuto <fujiya0101@gmail.com>
2023-07-28 16:43:20 +0900

    add:エラーページ

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { TodoListComponent } from './todo/todo-list/todo-list.component';
 import { AppComponent } from './app.component';
+import { ErrorComponent } from './error-dashboard/error/error.component';
 
 const routes: Routes = [
   { path: 'todo', component: TodoListComponent },
+  { path: 'error', component: ErrorComponent },
 ];
 
 @NgModule({
