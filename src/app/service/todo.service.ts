861b829f81193f7ef0fce5a8636e58bd4cd1f0d1
yasuto <fujiya0101@gmail.com>
2023-07-28 16:46:50 +0900

    add:todo取得 エラーハンリング実装

--- src/app/service/todo.service.ts
+++ src/app/service/todo.service.ts
@@ -1,8 +1,10 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { HttpClient, HttpHeaders , HttpErrorResponse} from '@angular/common/http';
+import { Observable,catchError,throwError } from 'rxjs';
 import { TodoResponse } from '../models/todo/TodoResponse.model';
 import { IS_ACTIVE,ACTIVE,IS_INACTIVE,State } from 'src/app/models/todo/State';
+import { environment } from 'src/environments/environment.development';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
@@ -10,13 +12,17 @@ import { IS_ACTIVE,ACTIVE,IS_INACTIVE,State } from 'src/app/models/todo/State';
 export class TodoService {
 
   constructor(
-    private http: HttpClient,
+    private http  : HttpClient,
+    private router: Router
   ) { }
 
-  private todoUrl = 'api/todos'
+  private todoUrl = `${environment.apiUrl}/todo`
 
   getTodos(): Observable<TodoResponse[]>{
-    return this.http.get<TodoResponse[]>(this.todoUrl)
+    return this.http.get<TodoResponse[]>(this.todoUrl).pipe
+    (
+      catchError(error => this.handleError(error,this.router))
+    )
   }
 
   toState(code:number):State {
@@ -32,5 +38,17 @@ export class TodoService {
     }
   }
 
+  private handleError(error: HttpErrorResponse,router:Router) {
+    if (error.status === 0) {
+      console.error('An error occurred:', error.error);
+    }else if(error.status === 500){
+      router.navigate(['error'])
+    }else {
+      console.error(
+        `Backend returned code`, error.error);
+    }
+    // Return an observable with a user-facing error message.
+    return throwError(() => new Error('Something bad happened; please try again later.'));
+  }
 
 }
