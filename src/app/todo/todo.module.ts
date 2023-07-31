502e64a6bcaa042f6ab592175cb658624c9cb8cf
yasuto <fujiya0101@gmail.com>
2023-07-31 20:55:56 +0900

    fix:commit漏れ

--- src/app/todo/todo.module.ts
+++ src/app/todo/todo.module.ts
@@ -1,17 +1,30 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { TodoRoutingModule } from './todo-routing.module';
 import { TodoListComponent } from './todo-list/todo-list.component';
 import { MatTableModule } from '@angular/material/table';
 import { MatButtonModule } from '@angular/material/button';
+import { MatSelectModule } from '@angular/material/select';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule} from '@angular/material/form-field';
+import { TodoCreateComponent } from './todo-create/todo-create.component';
+import { FormsModule,ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     TodoListComponent,
+    TodoCreateComponent,
   ],
   imports: [
     CommonModule,
+    TodoRoutingModule,
     MatTableModule,
     MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule,
+    FormsModule,
+    ReactiveFormsModule
   ]
 })
 export class TodoModule { }
