1116cf1bf869d124ce4d2d1c37c0b61d27ca87fe
yasuto <fujiya0101@gmail.com>
2023-07-31 20:49:44 +0900

    fix:ルーティングの切り分け

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,14 +3,16 @@ import { RouterModule, Routes } from '@angular/router';
 import { TodoListComponent } from './todo/todo-list/todo-list.component';
 import { AppComponent } from './app.component';
 import { ErrorComponent } from './error-dashboard/error/error.component';
+import { TodoModule } from './todo/todo.module';
 
 const routes: Routes = [
-  { path: 'todo', component: TodoListComponent },
+  { path: 'todo',
+    loadChildren:() => TodoModule},
   { path: 'error', component: ErrorComponent },
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes), TodoModule],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
