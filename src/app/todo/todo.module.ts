1337866c377ff93c4534c509c0f9d63bf1d89102
yasuto <fujiya0101@gmail.com>
2023-07-31 21:01:12 +0900

    fix:Module読み込み修正

--- src/app/todo/todo.module.ts
+++ src/app/todo/todo.module.ts
@@ -7,13 +7,11 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatSelectModule } from '@angular/material/select';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule} from '@angular/material/form-field';
-import { TodoCreateComponent } from './todo-create/todo-create.component';
 import { FormsModule,ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     TodoListComponent,
-    TodoCreateComponent,
   ],
   imports: [
     CommonModule,
