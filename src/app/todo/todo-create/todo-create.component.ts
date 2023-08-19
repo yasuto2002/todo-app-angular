c6b91c86d2d973195a1429e9fabf6a4d8f6b4125
yasuto <fujiya0101@gmail.com>
2023-08-19 19:30:30 +0900

    fix:Lacation ヘッダの内容変更に対応

--- src/app/todo/todo-create/todo-create.component.ts
+++ src/app/todo/todo-create/todo-create.component.ts
@@ -10,6 +10,7 @@ import { Router } from '@angular/router';
 import { HttpResponse } from '@angular/common/http';
 import { TodoIdResponse } from 'src/app/models/todo/TodoIdResponse.model';
 import { Subscription } from 'rxjs';
+import { environment } from 'src/environments/environment.development';
 
 @Component({
   selector: 'app-todo-create',
@@ -37,7 +38,7 @@ export class TodoCreateComponent implements OnInit, OnDestroy {
     const todoRequest: TodoRequest = this.todoFb.value as TodoRequest;
     this.subscription = this.todoService.addTodo(todoRequest).subscribe((response: HttpResponse<TodoIdResponse>) => {
       if(response.headers.get("Location") != null){
-        const newTodoId = new URL(response.headers.get("Location")!).pathname.split('/todo/').pop();
+        const newTodoId =  new URL(response.headers.get("Location")!,environment.apiUrl).pathname.split('/todo/').pop();
         this.router.navigate(["todo/update",newTodoId])
       }else{
         this.router.navigate(['error'],{ queryParams: { message: "Location header could not be read."}})
